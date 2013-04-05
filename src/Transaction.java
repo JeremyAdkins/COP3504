073ae3270f5d35f60e6d4509ae1c020d52d5d7db
Meyer Kizner <meyer@pre-alpha.com>
2013-04-05 14:59:34 -0400

    Add my DateTime and Time implementations in hw1.jar, and use them in Transaction

--- src/Transaction.java
+++ src/Transaction.java
@@ -1,39 +1,48 @@
+import hw1.DateTime;
+
 import java.math.BigDecimal;
 
+public final class Transaction {
+    private final Type type;
 
-public class Transaction {
-	private Type type;
-	private BigDecimal amount;
-//	private DateTime timestamp;
-	private FraudStatus fraudStatus;
-	
-	public enum Type{
-		DEPOSIT,INTEREST,WITHDRAWAL,FEE,CREATION;
-	}
-	public enum FraudStatus{
-		NOT_FLAGGED,FLAGGED,REVERSED;
-	}
-	
-	public Transaction(Type type, BigDecimal amount){
-		this.type = type;
-		this.amount = amount;
-//		this.timestamp = new DateTime();
-		this.fraudStatus = FraudStatus.NOT_FLAGGED;
-	}
-	
-	public Type getType(){
-		return type;
-	}
-	public BigDecimal getAmount(){
-		return amount;
-	}
-//	public DateTime getTimestamp(){
-//		return timestamp;
-//	}
-	public FraudStatus getFraudStatus(){
-		return fraudStatus;
-	}
-	public void setFraudStatus(FraudStatus fraudStatus){
-		this.fraudStatus=fraudStatus;
-	}
+    private final BigDecimal amount;
+
+    private final DateTime timestamp;
+
+    private FraudStatus fraudStatus;
+
+    public static enum Type {
+        DEPOSIT, INTEREST, WITHDRAWAL, FEE, CREATION;
+    }
+
+    public static enum FraudStatus {
+        NOT_FLAGGED, FLAGGED, REVERSED;
+    }
+
+    public Transaction(Type type, BigDecimal amount) {
+        this.type = type;
+        this.amount = amount;
+        this.timestamp = new DateTime();
+        this.fraudStatus = FraudStatus.NOT_FLAGGED;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public DateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public FraudStatus getFraudStatus() {
+        return fraudStatus;
+    }
+
+    public void setFraudStatus(FraudStatus fraudStatus) {
+        this.fraudStatus = fraudStatus;
+    }
 }
