fb1486d23fad47924cf7ca5d4df6badfec51dd81
Meyer Kizner <meyer@pre-alpha.com>
2013-04-14 11:25:52 -0400

    Add amortization code to Loan based on Josh's answer

--- src/project/model/Loan.java
+++ src/project/model/Loan.java
@@ -2,14 +2,35 @@ package project.model;
 
 import java.math.BigDecimal;
 
-public final class Loan extends AbstractLoan{
-	private final BigDecimal minimumPayment; // required to be paid by the end of the month
+public final class Loan extends AbstractLoan {
+    /**
+     * The base interest rate. This value is copied from the {@link PaymentSchedule} when the {@code Loan} is created, so
+     * that future interest rate changes do not affect the loan, and in particular, its minimum payment.
+     */
+    private final BigDecimal baseInterest;
+
+    /**
+     * The amount required to be paid towards the loan by the end of the month. If the minimum payment is not made, a
+     * penalty is applied to the account balance. Minimum payments are calculated based on the loan term and the interest
+     * rate.
+     */
+	private final BigDecimal minimumPayment;
 	
-	public Loan(BigDecimal amount, BigDecimal minimumPayment, BigDecimal interestPremium) throws InvalidInputException, LoanCapException {
+	public Loan(BigDecimal amount, int term, BigDecimal interestPremium) throws InvalidInputException, LoanCapException {
 		super(interestPremium);
         Bank.getInstance().authorizeLoan(amount);
 		applyTransaction(amount, Transaction.Type.WITHDRAWAL);
-		this.minimumPayment = minimumPayment;
+        if (term <= 0) {
+            throw new InvalidInputException(new BigDecimal(term), "loan term must be positive");
+        }
+
+        baseInterest = Bank.getInstance().getPaymentSchedule().getLoanInterest();
+        BigDecimal monthlyInterest = baseInterest.add(interestPremium).divide(new BigDecimal("12"), Bank.MATH_CONTEXT);
+        // see http://en.wikipedia.org/wiki/Amortization_calculator (the leftmost version of the equation)
+        BigDecimal exponentialTerm = monthlyInterest.add(BigDecimal.ONE).pow(term);
+        BigDecimal numerator = monthlyInterest.multiply(exponentialTerm);
+        BigDecimal denominator = exponentialTerm.subtract(BigDecimal.ONE);
+        minimumPayment = amount.multiply(numerator.divide(denominator, Bank.MATH_CONTEXT));
 	}
 
     @Override
@@ -40,7 +61,7 @@ public final class Loan extends AbstractLoan{
 	
 	@Override
 	protected BigDecimal getBaseInterest() {
-		return Bank.getInstance().getPaymentSchedule().getLoanInterest();
+		return baseInterest;
 	}
 
 	@Override
